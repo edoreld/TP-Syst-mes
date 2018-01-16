9a550c23f01da114df741a79c6cc9ede78c354c1
Francisco Javier Martinez Lago <edoreld@gmail.com>
2018-01-16 10:33:41 +0100

    added tests exo3

---
+++ src/exo3/Client.java
@@ -0,0 +1,20 @@
+package exo3;
+
+public class Client {
+	public Client() {
+
+	}
+
+	public static void main(String[] args) {
+		Handler h1 = new Employe(10, "Pedro");
+		Handler h2 = new Employe(20, "Maria");
+		Handler h3 = new Employe(30, "Gustavo");
+
+		h1.setSuccesor(h2);
+		h2.setSuccesor(h3);
+
+		h1.handleRequest(new Request(5));
+		h1.handleRequest(new Request(15));
+		h1.handleRequest(new Request(25));
+	}
+}
